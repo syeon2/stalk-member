1805fe0184c32ca3942f3215d3499f85e4db97ef
SUYEON KIM <waterkite94@gmail.com>
2024-07-22 22:16:51 +0900

    [Fix] insert redis host and port to lettuce

--- member-infrastructure/redis/src/main/kotlin/io/waterkite94/stalk/redis/config/RedisConfig.kt
+++ member-infrastructure/redis/src/main/kotlin/io/waterkite94/stalk/redis/config/RedisConfig.kt
@@ -2,13 +2,20 @@ package io.waterkite94.stalk.redis.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
-class RedisConfig {
+class RedisConfig(
+    private val env: Environment
+) {
+    private val host = env.getProperty("spring.data.redis.host", "localhost")
+    private val port = env.getProperty("spring.data.redis.port", "6379").toInt()
+
     @Bean
     fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> =
         RedisTemplate<String, Any>().apply {
@@ -20,5 +27,9 @@ class RedisConfig {
         }
 
     @Bean
-    fun lettuceConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory()
+    fun lettuceConnectionFactory(): RedisConnectionFactory {
+        val config = RedisStandaloneConfiguration(host, port)
+
+        return LettuceConnectionFactory(config)
+    }
 }
